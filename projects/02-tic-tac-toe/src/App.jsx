0a27d0fd6d2d526a1836f17b2df41296d7e7850e
Edison Rodriguez <67914096+edisondavidcode@users.noreply.github.com>
2026-05-02 16:55:45 -0400

    feat: implement main App component with game logic, state management, and localStorage persistence

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -5,9 +5,16 @@ import { checkWinnerFrom } from "./logic/board";
 import { WinnerModal } from "./components/WinnerModal";
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null));
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem("board");
+    if (boardFromStorage) return JSON.parse(boardFromStorage);
+    return Array(9).fill(null);
+  });
 
-  const [turn, setTurn] = useState(TURNS.X);
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem("turn");
+    if (turnFromStorage) return turnFromStorage;
+  });
 
   const [winner, setWinner] = useState(null);
 
@@ -15,11 +22,16 @@ function App() {
     setBoard(Array(9).fill(null));
     setWinner(null);
     setTurn(TURNS.X);
+    window.localStorage.removeItem("board");
+    window.localStorage.removeItem("turn");
   };
 
   const updateBoard = (index) => {
     const newBoard = [...board];
     const newTurn = turn == TURNS.X ? TURNS.O : TURNS.X;
+
+    window.localStorage.setItem("board", JSON.stringify(newBoard));
+    window.localStorage.setItem("turn", turn);
 
     if (newBoard[index] !== null || winner) return;
 
